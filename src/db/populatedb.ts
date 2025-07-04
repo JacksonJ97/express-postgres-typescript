768e5c3872ae4c95b95381b20519127cf18d8896
Jackson <jacksonjude97@gmail.com>
2025-07-04 17:59:01 -0400

    use tsx instead of ts-node and ts-nodde-dev

--- src/db/populatedb.ts
+++ src/db/populatedb.ts
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS usernames (
 
 INSERT INTO usernames (username) 
 VALUES
-  ('Bryan'),
-  ('Odin'),
-  ('Damon');
+  ('Alice'),
+  ('Bob'),
+  ('Charlie');
 `;
 
 async function main() {
